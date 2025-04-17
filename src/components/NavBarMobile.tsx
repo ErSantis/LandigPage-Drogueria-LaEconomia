a79c1fee82cff9ccf52975835924f121a4c7366a
Elkin <ersantis@uninorte.edu.co>
2025-04-17 17:25:47 -0500

    feat: add NavBarMobile component with section links and dropdown icon

---
+++ src/components/NavBarMobile.tsx
@@ -0,0 +1,27 @@
+import { DropdownIcon } from "./Icons"
+
+// NavBarMobile.tsx
+const NavBarMobile = () => {
+    const secciones = [
+        "Botiquín",
+        "Salud y belleza",
+        "Alimentos y bebidas",
+        "Varios"
+    ]
+
+    return (
+        <div className="flex flex-col">
+            {secciones.map((seccion, index) => (
+                <div className="h-12">
+                    <div key={index} className="h-full flex justify-between items-center py-2 px-5">
+                        <span className="font-poppins text-base font-semibold text-neutral-800">{seccion}</span>
+                        <DropdownIcon />
+                    </div>
+                    <hr className="border-gray-200" />
+                </div>
+            ))}
+        </div>
+    )
+}
+
+export default NavBarMobile
