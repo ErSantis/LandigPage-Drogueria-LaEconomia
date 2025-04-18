dbb61b7c4663c48cb81dac894dd4476cdfec10c9
Elkin <ersantis@uninorte.edu.co>
2025-04-17 22:40:53 -0500

    style: update NavigationButton component styles for improved layout and consistency

--- src/components/BannerSection/NavigationButton.tsx
+++ src/components/BannerSection/NavigationButton.tsx
@@ -1,6 +1,6 @@
-import { ChevronLeftIcon, ChevronRightIcon } from "../Icons";
-
 // components/BannerSection/NavigationButton.tsx
+import { ChevronLeftIcon, ChevronRightIcon } from '../Icons';
+
 interface NavigationButtonProps {
   direction: 'prev' | 'next';
   onClick: () => void;
@@ -11,17 +11,17 @@ export const NavigationButton = ({ direction, onClick }: NavigationButtonProps) 
     <button
       onClick={onClick}
       className={`
-        absolute top-1/2 -mt-[25px] z-10
+        absolute top-1/2 -mt-[15px] md:-mt-[25px] z-10
         flex justify-center items-center
-        w-[30px] h-[30px]
+        w-[20px] h-[20px] md:w-[30px] md:h-[30px]
         bg-[#1c58b7] text-white
-        rounded-2xl
+        rounded-xl md:rounded-2xl
         border border-[#194c9e]
         shadow-[1px_1px_4px_0_rgba(0,0,0,0.2)]
         font-poppins
         focus:outline-none
         hidden md:flex
-        ${direction === 'prev' ? 'left-[10px]' : 'right-[10px]'}
+        ${direction === 'prev' ? 'left-[5px] md:left-[10px]' : 'right-[5px] md:right-[10px]'}
       `}
     >
       {direction === 'prev' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
