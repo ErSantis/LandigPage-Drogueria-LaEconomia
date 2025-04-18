f560b9ba56c4498a1d90eecee41d861788cadd5a
Elkin <ersantis@uninorte.edu.co>
2025-04-18 16:46:54 -0500

    fix: correct formatting and add missing semicolons in NavBar component

--- src/components/HeaderSection/NavBar.tsx
+++ src/components/HeaderSection/NavBar.tsx
@@ -1,29 +1,29 @@
-import { ChevronDown } from "../Icons"
+import { ChevronDown } from "../Icons";
 
 const NavBar = () => {
-
     const secciones = [
         "Medicamentos",
         "Botiquin",
         "Salud y Belleza",
         "Cuidados del bebé",
         "Alimentos y bebidas",
-        "Varios"
-    ]
+        "Varios",
+    ];
     return (
-        <nav className="w-full h-10 bg-blue-700 text-white px-20 py-2">
-
+        <nav className="w-full h-10 bg-blue-700 text-white px-20 py-2 hidden md:block">
             <ul className="w-full  h-full flex items-center gap-8 text-sm">
                 {secciones.map((seccion, index) => (
-                    <li key={index} className="gap-2 flex items-center font-poppins text-sm font-medium ">
+                    <li
+                        key={index}
+                        className="gap-2 flex items-center font-poppins text-sm font-medium "
+                    >
                         <span>{seccion}</span>
                         <ChevronDown />
                     </li>
                 ))}
             </ul>
-
         </nav>
-    )
-}
+    );
+};
 
-export default NavBar
+export default NavBar;
