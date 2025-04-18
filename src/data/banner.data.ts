8004cc0ecb0587363a3098e33787bbb9be5a5c03
Elkin <ersantis@uninorte.edu.co>
2025-04-17 22:41:03 -0500

    style: standardize quotes and formatting in banner data for consistency

--- src/data/banner.data.ts
+++ src/data/banner.data.ts
@@ -1,24 +1,42 @@
 // data/banners.ts
-import { Banner } from '../types/banner.type';
+import { Banner } from "../types/banner.type";
 
 export const banners: Banner[] = [
   {
     id: 1,
-    image: '/images/banner1.png',
-    alt: 'Promoción 1',
-    link: '/promo1'
+    image: "/images/banner1.png",
+    alt: "Promoción 1",
+    link: "/promo1",
   },
   {
     id: 2,
-    image: '/images/banner2.png',
-    alt: 'Promoción 2',
-    link: '/promo2'
+    image: "/images/banner2.png",
+    alt: "Promoción 2",
+    link: "/promo2",
   },
 
-    {
-        id: 3,
-        image: '/images/banner3.png',
-        alt: 'Promoción 3',
-        link: '/promo3'
-    }
+  {
+    id: 3,
+    image: "/images/banner3.png",
+    alt: "Promoción 3",
+    link: "/promo3",
+  },
+  {
+    id: 4,
+    image: "/images/banner4.png",
+    alt: "Promoción 4",
+    link: "/promo4",
+  },
+  {
+    id: 5,
+    image: "/images/banner5.png",
+    alt: "Promoción 5",
+    link: "/promo5",
+  },
+  {
+    id: 6,
+    image: "/images/banner6.png",
+    alt: "Promoción 6",
+    link: "/promo6",
+  },
 ];
