179886d31ca580c3cc769268346af5e9e9d54a27
Elkin <ersantis@uninorte.edu.co>
2025-04-18 16:47:23 -0500

    fix: adjust styling and class names for consistency in FeaturedProduct component

--- src/components/FeaturedHomeSection/FeaturedProduct.tsx
+++ src/components/FeaturedHomeSection/FeaturedProduct.tsx
@@ -9,18 +9,18 @@ interface FeaturedProductProps {
 
 const FeaturedProduct = React.memo(({ title, image }: FeaturedProductProps) => {
     return (
-        <Link 
-            to="" 
+        <Link
+            to=""
             className="relative flex flex-col items-center gap-2 w-full justify-center group no-underline"
         >
-            <div className="relative flex items-center justify-center w-[100px] h-[100px] gap-[8.4px] rounded-full border-[3.36px] border-solid border-[#9EC8F1] mx-auto">
+            <div className="relative flex items-center justify-center rounded-full  border-solid border-blue-300 w-[60px] h-[60px] border-[2.02px] gap-[5.04px] md:w-[100px] md:h-[100px] md:border-[3.36px] md:gap-[8.4px] ">
                 <img
                     src={image}
                     alt={title}
-                    className="absolute w-auto h-auto max-w-[120%] max-h-[120%] transform -translate-y-2 transition-all duration-300 ease-in-out group-hover:scale-110"
+                    className="absolute w-auto h-auto max-w-[120%] max-h-[120%] transform  transition-all duration-300 ease-in-out group-hover:scale-110"
                 />
             </div>
-            <span className="text-sm font-medium text-gray-900 text-center">{title}</span>
+            <span className="text-xs font-normal text-gray-900 text-center md:text-base md:font-medium">{title}</span>
         </Link>
     );
 });
