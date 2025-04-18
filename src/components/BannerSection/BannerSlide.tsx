0caacb833a4a4ed09dd8a3521e1becdbad5ac5b0
Elkin <ersantis@uninorte.edu.co>
2025-04-17 22:40:48 -0500

    style: adjust formatting and add rounded styles to images in BannerSlide component

--- src/components/BannerSection/BannerSlide.tsx
+++ src/components/BannerSection/BannerSlide.tsx
@@ -2,17 +2,16 @@
 import { Banner } from '../../types/banner.type';
 
 interface BannerSlideProps {
-  banner: Banner;
+    banner: Banner;
 }
-
 export const BannerSlide = ({ banner }: BannerSlideProps) => {
-  return (
-    <a href={banner.link} className="block w-full h-full">
-      <img
-        src={banner.image}
-        alt={banner.alt}
-        className="w-full h-full object-cover"
-      />
-    </a>
-  );
-};
+    return (
+      <a href={banner.link} className="block w-full h-full">
+        <img
+          src={banner.image}
+          alt={banner.alt}
+          className="w-full h-full object-cover rounded-lg md:rounded-none"
+        />
+      </a>
+    );
+  };
