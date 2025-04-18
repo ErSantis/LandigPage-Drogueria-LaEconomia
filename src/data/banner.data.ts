ebc4b031150a0cdf8cd6b689f46d7b4d135bb97f
Elkin <ersantis@uninorte.edu.co>
2025-04-17 21:47:21 -0500

    feat: add banner data for promotional images

---
+++ src/data/banner.data.ts
@@ -0,0 +1,24 @@
+// data/banners.ts
+import { Banner } from '../types/banner.type';
+
+export const banners: Banner[] = [
+  {
+    id: 1,
+    image: '/images/banner1.png',
+    alt: 'Promoción 1',
+    link: '/promo1'
+  },
+  {
+    id: 2,
+    image: '/images/banner2.png',
+    alt: 'Promoción 2',
+    link: '/promo2'
+  },
+
+    {
+        id: 3,
+        image: '/images/banner3.png',
+        alt: 'Promoción 3',
+        link: '/promo3'
+    }
+];
