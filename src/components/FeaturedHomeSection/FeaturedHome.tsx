74d1ee4ea7e17dbfb80c3aa65ab93d0c8c060b0b
Elkin <ersantis@uninorte.edu.co>
2025-04-18 01:03:48 -0500

    feat: implement Swiper for featured products display in FeaturedHome component

--- src/components/FeaturedHomeSection/FeaturedHome.tsx
+++ src/components/FeaturedHomeSection/FeaturedHome.tsx
@@ -1,14 +1,49 @@
-
+// FeaturedHome.tsx
 import FeaturedProduct from "./FeaturedProduct"
 import { FeturedSections } from "../../data/featuredSection.data"
-const FeaturedHome = () => {
+import { Swiper, SwiperSlide } from 'swiper/react';
 
+// Importar estilos de Swiper
+import 'swiper/swiper-bundle.css';
+
+const FeaturedHome = () => {
     return (
-        <div className="h-33 w-full bg-white flex items-center gap-2 px-40 mt-4">
-            {FeturedSections.map((section, index) => (
-                <FeaturedProduct key={index} title={section.title} image={section.image} />
-            ))}
+        <div className="w-full bg-white mt-4">
+            <div className="px-4 md:px-40">
+                <Swiper
+                    loop={true}
+                    spaceBetween={20}
+                    slidesPerView={6}
+                    allowTouchMove={true}
+                    style={{ padding: '20px 0' }} // Añade padding vertical
+                    breakpoints={{
+                        320: {
+                            slidesPerView: 3.5,
+                            spaceBetween: 12,
+                        },
+                        768: {
+                            slidesPerView: 4,
+                            spaceBetween: 20,
+                        },
+                        1024: {
+                            slidesPerView: 6,
+                            spaceBetween: 20,
+                        },
+                    }}
+                    className="!overflow-y-visible" // Permite que el contenido se desborde verticalmente
+                >
+                    {FeturedSections.map((section, index) => (
+                        <SwiperSlide key={index} className="!h-auto"> {/* Altura automática */}
+                            <FeaturedProduct
+                                title={section.title}
+                                image={section.image}
+                            />
+                        </SwiperSlide>
+                    ))}
+                </Swiper>
+            </div>
         </div>
     )
 }
+
 export default FeaturedHome
