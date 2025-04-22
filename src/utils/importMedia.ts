6ffb7bae6d71a8a9045329389872b55ef47d0cc4
Elkin <ersantis@uninorte.edu.co>
2025-04-21 20:18:07 -0500

    feat: add importIcons function to streamline icon asset loading

--- src/utils/importMedia.ts
+++ src/utils/importMedia.ts
@@ -3,3 +3,7 @@ export const importImage = (imgName: string): string =>
 
 export const importVideo = (videoName: string): string =>
   new URL(`../assets/videos/${videoName}`, import.meta.url).href;
+
+export const importIcons = (iconName: string): string =>
+  new URL(`../assets/icons/${iconName}`, import.meta.url).href;
+
