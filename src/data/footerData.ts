e9160313beb8bda82843dd15946a3a00e1a3fd8f
Elkin <ersantis@uninorte.edu.co>
2025-04-22 19:25:39 -0500

    feat: add Footer component with structured sections and dynamic links; include new icons and images; update package dependencies

---
+++ src/data/footerData.ts
@@ -0,0 +1,35 @@
+export const footerData = [
+    {
+        title: "La empresa",
+        links: [
+            { text: "Quiénes somos", to: "#" },
+            { text: "Misión, Visión e Historia", to: "#" },
+            { text: "Ética", to: "#" },
+            { text: "Nuestras Sucursales", to: "#" },
+        ],
+    },
+    {
+        title: "Información",
+        links: [
+            { text: "Aviso de Privacidad", to: "#" },
+            { text: "Habeas Data", to: "#" },
+            { text: "Términos y condiciones de promociones", to: "#", breakWords: true },
+        ],
+    },
+    {
+        title: "Club Vida Sana",
+        links: [
+            { text: "Beneficios del Club", to: "#" },
+            { text: "Ley 2300", to: "#" },
+            { text: "Consulta Aliados", to: "#" },
+        ],
+    },
+    {
+        title: "Contacto",
+        links: [
+            { text: "315 782 3477", to: "#" },
+            { text: "Radicar PQRS", to: "#" },
+            { text: "servicioalcliente@droguerialaeconomia.com" ,to: "#",breakWords: true },
+        ],
+    },
+];
